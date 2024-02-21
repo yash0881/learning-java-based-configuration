6be60a9d4d100c2bc314056a85ce7ed1f124a823
Yash Gupta <yash.itca24@gmail.com>
2024-02-22 00:11:50 +0530

    creating multiple beans of single class using java confiura tion file

--- src/main/java/org/java/configuration/Main.java
+++ src/main/java/org/java/configuration/Main.java
@@ -1,6 +1,5 @@
 package org.java.configuration;
 
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -10,7 +9,11 @@ public class Main {
         System.out.println("Hello world!");
 
         ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfigFile.class);
-        Student s = context.getBean(Student.class);
+        Student s = (Student) context.getBean("s1"); // Explicit cast to Student
         System.out.println(s);
+
+        System.out.println("_______________________________________________");
+        Student student = (Student) context.getBean("s2"); // Explicit cast to Student
+        System.out.println(student);
     }
 }
--- src/main/java/org/java/configuration/SpringConfigFile.java
+++ src/main/java/org/java/configuration/SpringConfigFile.java
@@ -6,12 +6,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfigFile {
 
-    @Bean
-    public Student std() {
+    @Bean("s1")
+    public Student first() {
         Student s = new Student();
         s.setRoll(102);
         s.setName("Suraj");
         s.setId(1002);
+        return s;
+    }
+
+    @Bean("s2")
+    public Student second(){
+        Student s = new Student();
+        s.setRoll(103);
+        s.setName("Ram");
+        s.setId(1004);
         return s;
     }
 }
